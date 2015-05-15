d307a92e7f06ff5b3e7c14869b2b6241d331dbf7
kazuki-mo <moriya.kazuki.me0@is.naist.jp>
2015-05-15 13:54:58 +0900

    Error Commit

--- TestMacApp01/AppDelegate.swift
+++ TestMacApp01/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var TB_Test: NSTextField!
     
     @IBAction func BT_Test(sender: AnyObject) {
-        TB_Test.stringValue = "Change!"
+        TB_Test.stringValue = "Error"
     }
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
