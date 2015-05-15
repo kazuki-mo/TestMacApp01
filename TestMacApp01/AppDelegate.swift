2f989cf75dd8b7050478ecdb1db153496236db00
kazuki-mo <moriya.kazuki.me0@is.naist.jp>
2015-05-15 11:01:09 +0900

    Initial Commit

---
+++ TestMacApp01/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TestMacApp01
+//
+//  Created by 守谷 一希 on 2015/05/15.
+//  Copyright (c) 2015年 守谷 一希. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
